90ec35298a2e56aa5af214b9e94701025c912a2f
Michael Bausor <michaelbausor@google.com>
2019-06-25 09:18:27 -0700

    Update synth.py for Language

--- google-cloud-clients/google-cloud-language/synth.py
+++ google-cloud-clients/google-cloud-language/synth.py
@@ -18,22 +18,19 @@ import synthtool as s
 import synthtool.gcp as gcp
 import synthtool.languages.java as java
 
-gapic = gcp.GAPICGenerator()
+gapic = gcp.GAPICCloudBuild()
 
 service = 'language'
 versions = ['v1', 'v1beta2']
-config_pattern = '/google/cloud/language/artman_language_{version}.yaml'
 
 for version in versions:
     library = gapic.java_library(
         service=service,
-        version=version,
-        config_path=config_pattern.format(version=version),
-        artman_output_name='')
+        version=version)
 
-    s.copy(library / f'gapic-google-cloud-{service}-{version}/src', 'src')
-    s.copy(library / f'grpc-google-cloud-{service}-{version}/src', f'../../google-api-grpc/grpc-google-cloud-{service}-{version}/src')
-    s.copy(library / f'proto-google-cloud-{service}-{version}/src', f'../../google-api-grpc/proto-google-cloud-{service}-{version}/src')
+    s.copy(library / f'gapic-google-cloud-{service}-{version}-java/src', 'src')
+    s.copy(library / f'grpc-google-cloud-{service}-{version}-java/src', f'../../google-api-grpc/grpc-google-cloud-{service}-{version}/src')
+    s.copy(library / f'proto-google-cloud-{service}-{version}-java/src', f'../../google-api-grpc/proto-google-cloud-{service}-{version}/src')
 
     java.format_code('./src')
     java.format_code(f'../../google-api-grpc/grpc-google-cloud-{service}-{version}/src')
